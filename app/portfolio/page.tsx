0e26895198ae26ede9e520dfc919701fa23f28dc
Michelle <michellewhite919@gmail.com>
2024-02-28 12:52:23 -0500

    fixed

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -21,10 +21,7 @@ export default function Page() {
   );
 
   const [addFormOn, setAddFormOn] = useState(false);
-<<<<<<< HEAD
-=======
   const { currency, user, userSession,  palette, mode} = useCrypto();
->>>>>>> da9b3dfa2f5a4ea5fdcd27e075f8800dd69d6800
   const portCoins = useAppSelector((state: RootState) => state.portfolio.coins);
   const dispatch = useDispatch();
 
@@ -32,11 +29,6 @@ export default function Page() {
     setAddFormOn(!addFormOn);
   };
 
-<<<<<<< HEAD
-  return (
-    <div className="w-full flex items-center justify-center flex-col mt-20">
-      <div className="w-full flex justify-end my-8 mr-36">
-=======
   if ((!user && !userSession) || user === null) {
     router.push("/sign-up");
   }
@@ -46,7 +38,6 @@ export default function Page() {
       className={`w-screen h-screen bg-base theme-${palette} theme-${mode} flex items-center justify-start flex-col  `}
     >
       <div className="w-full flex justify-end my-8 mr-36 mt-36">
->>>>>>> da9b3dfa2f5a4ea5fdcd27e075f8800dd69d6800
         <button
           className="bg-accent p-4 rounded-lg"
           onClick={() => setAddFormOn(!addFormOn)}
