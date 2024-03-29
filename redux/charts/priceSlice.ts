1fa7657e695cb6a8294480d47e6b2026f31a974c
Michelle <michellewhite919@gmail.com>
2024-03-29 12:21:16 -0400

    update

--- redux/charts/priceSlice.ts
+++ redux/charts/priceSlice.ts
@@ -1,7 +1,6 @@
 "use client";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { addChartCoin } from "@/redux/charts/chartsSlice";
-import { ChartCoin } from "@/app/sharedinterfaces";
 
 export const priceChart = createAsyncThunk(
   "priceChart",
@@ -24,47 +23,8 @@ export const priceChart = createAsyncThunk(
         time: days,
         prices: json.prices,
         volume: json.total_volumes,
+        yData: json.prices,
       })
     );
   }
 );
-
-const initialState = {
-  chartCoins: [],
-  days: "",
-  prices: [],
-  total_volume: [],
-};
-
-const priceChartSlice = createSlice({
-  name: "priceChart",
-  initialState,
-  reducers: {
-    setDays: (state, action) => {
-      state.days = action.payload;
-    },
-    setPrices: (state, action) => {
-      state.prices = action.payload;
-    },
-    setVolume: (state, action) => {
-      state.total_volume = action.payload;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(priceChart.pending, (state, action) => {
-        state.loading = true;
-      })
-      .addCase(priceChart.fulfilled, (state, action) => {
-        state.chartCoins = action.payload;
-        state.loading = false;
-      })
-      .addCase(priceChart.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message ?? "An unkown error occurrfetchData";
-      });
-  },
-});
-
-export const { setDays, setVolume, setPrices } = priceChartSlice.actions;
-export default priceChartSlice.reducer;
