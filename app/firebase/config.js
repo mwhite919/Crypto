4de0689ee33348d26389804c7330a772f8cedd47
Michelle <michellewhite919@gmail.com>
2024-03-29 21:07:09 -0400

    updat3e

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -18,6 +18,7 @@ export default function initializeFirebase() {
   const db = getFirestore(app);
   return { app, auth, db };
 }
+git;
 
 export function signup(email, password) {
   const { auth } = initializeFirebase();
